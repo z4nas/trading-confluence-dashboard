ca96c6db383969477e361cd15e74d1563ec6e027
z4nas <rbrinsonjr.rb@gmail.com>
2024-08-14 22:50:18 -0500

    Delete backend directory

--- backend/models/Indicator.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const indicatorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Indicator', indicatorSchema);
